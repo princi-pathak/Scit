aaeab340a29ba07073b530b4c77974963e6fcd53
komalgautm <mobappssolutions168@gmail.com>
2024-09-02 18:18:54 +0530

    work on crm in lead full history

--- app/Models/CRMLeadTask.php
+++ app/Models/CRMLeadTask.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class CRMLeadTask extends Model
 {
@@ -30,5 +31,14 @@ class CRMLeadTask extends Model
         'task_time',
         'notes'
     ];
+
+    public static function getCRMLeadTaskData($lead_id, $home_id){
+        return DB::table('crm_lead_tasks')
+        ->join('lead_tasks', 'lead_tasks.id', '=', 'crm_lead_tasks.task_type_id')
+        ->select('crm_lead_tasks.*', 'lead_tasks.title as lead_task_title')
+        ->where(['crm_lead_tasks.lead_id'=> $lead_id, 'crm_lead_tasks.home_id' => $home_id])
+        ->get();
+
+    }
 
 }
