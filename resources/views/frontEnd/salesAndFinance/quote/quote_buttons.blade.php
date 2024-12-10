3ef0c6af49f7829b97f5f2ebd5ca5888d7a47870
komalgautm <mobappssolutions168@gmail.com>
2024-12-10 18:12:53 +0530

    work on attachment section

--- resources/views/frontEnd/salesAndFinance/quote/quote_buttons.blade.php
+++ resources/views/frontEnd/salesAndFinance/quote/quote_buttons.blade.php
@@ -1,8 +1,8 @@
 <div class="row">
     <div class="col-md-8 col-lg-8 col-xl-8 px-3">
         <div class="jobsection">
-            <a href="#" class="profileDrop">New Quotes</a>
-            <a href="#" class="profileDrop">Draft <span>(5)</span></a>
+            <a href="{{ url('/quote/add') }}" class="profileDrop">New Quotes</a>
+            <a href="{{ url('/quote/draft') }}" class="profileDrop">Draft <span>({{ $draftCount }})</span></a>
             <a href="#" class="profileDrop">Actioned<span>(8)</span></a>
             <a href="#" class="profileDrop">Converted<span>(15)</span></a>
             <a href="#" class="profileDrop">Call back<span>(76)</span></a>
