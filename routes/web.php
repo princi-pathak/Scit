284bbc6ebe61a44bbce701b81abeef32fd990799
Ram Kumar <mobappssolutions156@gmail.com>
2025-01-04 17:59:35 +0530

    purchase order test fix bugs

--- routes/web.php
+++ routes/web.php
@@ -399,6 +399,8 @@ Route::group(['middleware' => ['checkUserAuth', 'lock']], function () {
 		Route::post('/searchSupplier','searchSupplier');
 		Route::post('/searchCreatedBy','searchCreatedBy');
 		Route::post('/searchProject','searchProject');
+		Route::post('/searchPurchase_qoute_ref','searchPurchase_qoute_ref');
+		Route::post('/searchPurchase_job_ref','searchPurchase_job_ref');
 	});
 	
 	// Forontend Customer Controller
