a1dceb7ecd176f620764997c124fbd58d2e62600
komalgautm <mobappssolutions168@gmail.com>
2025-05-02 18:08:56 +0530

    working on sales purchase feature in backend

--- app/Http/Controllers/backEnd/salesfinance/DayBook/PurchaseBackendController.php
+++ app/Http/Controllers/backEnd/salesfinance/DayBook/PurchaseBackendController.php
@@ -4,23 +4,154 @@ namespace App\Http\Controllers\backEnd\salesfinance\DayBook;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\Supplier;
+use App\Models\Construction_tax_rate;
+use App\Models\PurchaseExpenses;
+use Illuminate\Support\Carbon;
+use App\Http\Requests\Daybook\PurchaseDayBookRequest;
+use App\Models\DayBook\PurchaseDayBook;
+use App\Home;
+use App\Models\DayBook\SalesDayBook;
 class PurchaseBackendController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $data['page'] = "dayBook";
         return view('backEnd.salesFinance.DayBook.purchase_day_book', $data);
     }
-    public function purchase_type(){
+    public function purchase_type()
+    {
         $data['page'] = "purchaseExpenese";
+        $data['purchase_expenses'] = PurchaseExpenses::where('deleted_at', null)->get();
         return view('backEnd.salesFinance.DayBook.purchase_expenses', $data);
     }
-    public function create(){
+    public function create()
+    {
         $data['page'] = "dayBook";
         return view('backEnd.salesFinance.DayBook.purchase_day_book_form', $data);
     }
-    public function purchase_type_add(){
+    public function purchase_type_add()
+    {
         $data['page'] = "purchaseExpenese";
         return view('backEnd.salesFinance.DayBook.purchase_expenses_form', $data);
     }
+
+    public function save_purchase_expenses_type(Request $request)
+    {
+        $data['page'] = "purchaseExpenese";
+
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'status' => 'required|boolean',
+        ]);
+
+        $data = PurchaseExpenses::updateOrCreate(['id' => $request['purchase_expense_id']], $validatedData);
+
+        if (empty($request['purchase_expense_id'])) {
+            if ($data) {
+                return redirect()->route('backend.purchase_expenses_type')->with('success', 'Purchase expenses type added successfully!');
+            } else {
+                return redirect()->route('backend.purchase_expenses_type_add')->with('error', 'Failed to save purchase expenses type');
+            }
+        } else {
+            if ($data) {
+                return redirect()->route('backend.purchase_expenses_type')->with('success', 'Purchase expenses type edited successfully!');
+            } else {
+                return redirect()->route('backend.purchase_expenses_type_add')->with('error', 'Failed to edit purchase expenses type');
+            }
+        }
+    }
+
+    public function changeStatus(Request $request)
+    {
+        $data = PurchaseExpenses::find($request->id);
+        if ($data) {
+            $data->status = !$data->status;
+            $data->save();
+            return response()->json(['success' => true, 'message' => 'Status changed successfully!']);
+        } else {
+            return response()->json(['success' => false, 'message' => 'Failed to change status']);
+        }
+    }
+
+    public function deletePurchaseExpensesType($id)
+    {
+        try {
+            $expense = PurchaseExpenses::findOrFail($id); // Ensure the record exists
+            $expense->delete(); // Delete the record
+            return redirect()->back()->with('success', 'Purchase expense type deleted successfully.');
+        } catch (\Exception $e) {
+            return redirect()->back()->with('error', 'Failed to delete purchase expense type.');
+        }
+    }
+    public function editPurchaseExpensesType($id)
+    {
+        $data['page'] = "dayBook";
+        $data['purchase_expenses'] = PurchaseExpenses::findOrFail($id);
+        return view('backEnd.salesFinance.DayBook.purchase_expenses_form', $data);
+    }
+
+    public function getSupplierData(){
+        $data = Supplier::getActiveSuppliers(Auth::user()->home_id, Auth::user()->id);
+
+        return response()->json([
+            'success' => (bool) $data,
+            'data' => $data ? $data : 'No data'
+        ]);
+    }
+
+    public function getPurchaseExpense(){
+        $data = PurchaseExpenses::where('deleted_at', null)->where('status', true)->get();
+        return response()->json([
+            'success' => (bool) $data,
+            'data' => $data ? $data : 'No data'
+        ]);
+    }
+
+    public function getActiveTaxRate(){
+        $data = Construction_tax_rate::getAllTax_rate(Auth::user()->home_id, "Active");
+
+        return response()->json([
+            'success' => (bool) $data,
+            'data' => $data ? $data : 'No data.'
+        ]);
+    }
+
+    public function store(PurchaseDayBookRequest $request)
+    {
+        $data = $request->validated();
+        $data['page'] = "dayBook";
+        $convertedDate = Carbon::createFromFormat('d-m-Y', $data['date'])->format('Y-m-d');
+        $data['date'] = $convertedDate;
+        $data['home_id'] = Auth::user()->home_id;
+        $response = PurchaseDayBook::updateOrCreate(['id' => $data['purchase_day_book_id'] ?? null], $data);
+
+        if ($response->wasRecentlyCreated) {
+            return response()->json([  'success' => true, 'message' => 'Purchase day book record created successfully!', 'data' => $response], 201);
+        } elseif ($response->wasChanged()) {
+            return response()->json([  'success' => true, 'message' => 'Purchase day book record updated successfully!', 'data' => $response], 200);
+        } else {
+            return response()->json([  'success' => false, 'message' => 'No changes made.', 'data' => $response], 200);
+        }
+    }
+
+    public function purchase_day_book_reclaim_per(){
+        return Home::where('id', Auth::user()->home_id)->value('is_registered');
+    }
+    
+    public function reclaimPercantage(){
+
+        $excepmt = SalesDayBook::Where('Vat', 1)->whereNull('deleted_at')->sum('netAmount');
+        $standard = SalesDayBook::Where('Vat', 2)->whereNull('deleted_at')->sum('netAmount');
+
+        $residual = (($standard) / ($standard + $excepmt))*100;
+        // $reclaim  = round($residual, 2); 
+
+        return response()->json([
+            'success' => (bool) $excepmt,
+            'data' => $residual ? $residual : 0
+        ]);
+    }  
+    
 }
--- resources/views/backEnd/salesFinance/DayBook/purchase_expenses_form.blade.php
+++ resources/views/backEnd/salesFinance/DayBook/purchase_expenses_form.blade.php
@@ -10,23 +10,41 @@
             <div class="col-lg-12">
                 <section class="panel">
                     <header class="panel-heading">
-                        Purchase Expenses Add
+                        Purchase Expenses Type Add
                     </header>
+                    @if(session('success'))
+                    <div class="alert alert-success">{{ session('success') }}</div>
+                    @endif
+
+                    @if(session('error'))
+                    <div class="alert alert-danger">{{ session('error') }}</div>
+                    @endif
+
+                    @if ($errors->any())
+                    <div class="alert alert-danger">
+                        <ul>
+                            @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                            @endforeach
+                        </ul>
+                    </div>
+                    @endif
+
                     <div class="panel-body">
                         <div class="position-center">
-                            <form class="form-horizontal" role="form">
+                            <form class="form-horizontal" action="{{ url('admin/sales-finance/purchase/save-purchase-expenses-type') }}" role="form">
                                 <div class="form-group">
                                     <label for="" class="col-lg-2 col-sm-2 control-label">Title</label>
                                     <div class="col-lg-10">
-                                        <input type="text" class="form-control" id="" placeholder="Title">
+                                        <input type="text" name="title" class="form-control" id="" value="{{ isset($purchase_expenses) ? $purchase_expenses->title : '' }}" placeholder="Title">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label for="" class="col-lg-2 col-sm-2 control-label">Status</label>
                                     <div class="col-lg-10">
-                                        <select class="form-control">
-                                            <option value="1">Active</option>
-                                            <option value="2">Inactive</option>
+                                        <select class="form-control" name="status">
+                                            <option value="1" {{ isset($purchase_expenses) && $purchase_expenses->status == 1 ? 'selected' : '' }}>Active</option>
+                                            <option value="0" {{ isset($purchase_expenses) && $purchase_expenses->status == 0 ? 'selected' : '' }}>Inactive</option>
                                         </select>
                                     </div>
                                 </div>
