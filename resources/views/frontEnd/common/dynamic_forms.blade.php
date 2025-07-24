49142b561fd2e594957ed996e87f5b1938185d3b
komalgautm <mobappssolutions168@gmail.com>
2025-07-24 18:32:13 +0530

    working on daily log shows a single log for daily weekly and monthly

--- resources/views/frontEnd/common/dynamic_forms.blade.php
+++ resources/views/frontEnd/common/dynamic_forms.blade.php
@@ -43,7 +43,7 @@ $service_user_id = (isset($service_user_id)) ? $service_user_id : 0;
                     <div class="add-new-box risk-tabs custm-tabs">
                         <form method="post" action="" id="TopForm">
                             <div class="form-group col-md-12 col-sm-12 col-xs-12 p-0">
-                                <label class="col-md-1 col-sm-1 col-xs-12 p-t-7">User: </label>
+                                <label class="col-md-1 col-sm-1 col-xs-12 p-t-7">Child: </label>
                                 <div class="col-md-11 col-sm-11 col-xs-12">
                                     <div class="select-style">
                                         <select name="service_user_id" class="su_n_id">
